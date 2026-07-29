d7397af730c01294f5f128262aaa0e42d23dd7df
Matt Bowers <m.a.bowers@stonetable.org.uk>
2026-07-29 18:23:06 +0100

    Remove install scripts from source control

--- install-live.sh
+++
@@ -1,3 +0,0 @@
-cp main.js /Users/matthew/Documents/Vaults/Live/.obsidian/plugins/chord-sheets-mb/
-cp manifest.json /Users/matthew/Documents/Vaults/Live/.obsidian/plugins/chord-sheets-mb/
-cp styles.css /Users/matthew/Documents/Vaults/Live/.obsidian/plugins/chord-sheets-mb/
--- install.sh
+++
@@ -1,4 +0,0 @@
-cp main.js /Users/matthew/Documents/Vaults/Stonetable/.obsidian/plugins/chord-sheets-mb/
-cp manifest.json /Users/matthew/Documents/Vaults/Stonetable/.obsidian/plugins/chord-sheets-mb/
-cp styles.css /Users/matthew/Documents/Vaults/Stonetable/.obsidian/plugins/chord-sheets-mb/
-cp .hotreload /Users/matthew/Documents/Vaults/Stonetable/.obsidian/plugins/chord-sheets-mb/
